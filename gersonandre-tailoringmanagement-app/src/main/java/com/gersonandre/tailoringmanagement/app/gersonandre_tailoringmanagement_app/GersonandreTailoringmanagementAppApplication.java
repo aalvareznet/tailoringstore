a355b5a5feb2ca31ceb7dc7f48dacecb4181271d
Anthony Alvarez <velurn123@gmail.com>
2025-05-01 19:24:23 -0600

    feat: add initial Spring Boot application setup with Maven and necessary configurations

---
+++ gersonandre-tailoringmanagement-app/src/main/java/com/gersonandre/tailoringmanagement/app/gersonandre_tailoringmanagement_app/GersonandreTailoringmanagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.gersonandre.tailoringmanagement.app.gersonandre_tailoringmanagement_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GersonandreTailoringmanagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GersonandreTailoringmanagementAppApplication.class, args);
+	}
+
+}
---
+++ gersonandre-tailoringmanagement-app/src/test/java/com/gersonandre/tailoringmanagement/app/gersonandre_tailoringmanagement_app/GersonandreTailoringmanagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gersonandre.tailoringmanagement.app.gersonandre_tailoringmanagement_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GersonandreTailoringmanagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
