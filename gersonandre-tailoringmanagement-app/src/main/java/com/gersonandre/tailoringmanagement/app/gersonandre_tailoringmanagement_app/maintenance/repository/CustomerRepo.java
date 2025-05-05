c018c32c768f272907f3eddf747b23941878e2bb
Anthony Alvarez <velurn123@gmail.com>
2025-05-05 13:48:02 -0600

    feat: update CustomerRepo to use correct entity type and add docker-compose configuration for MySQL service

--- gersonandre-tailoringmanagement-app/src/main/java/com/gersonandre/tailoringmanagement/app/gersonandre_tailoringmanagement_app/maintenance/repository/CustomerRepo.java
+++ gersonandre-tailoringmanagement-app/src/main/java/com/gersonandre/tailoringmanagement/app/gersonandre_tailoringmanagement_app/maintenance/repository/CustomerRepo.java
@@ -3,7 +3,9 @@ package com.gersonandre.tailoringmanagement.app.gersonandre_tailoringmanagement_
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gersonandre.tailoringmanagement.app.gersonandre_tailoringmanagement_app.maintenance.entity.Customer;
+
 @Repository
-public interface CustomerRepo extends JpaRepository<CustomerRepo, Long>{
+public interface CustomerRepo extends JpaRepository<Customer, Long>{
 
 }
